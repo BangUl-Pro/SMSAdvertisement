f833a5295240724c0eb1443cf9846a8eaa3ed035
WindArrow <wind.dong.dream@gmail.com>
2015-10-19 17:34:56 +0900

    Zz

--- hello-world-server.js
+++ hello-world-server.js
@@ -67,7 +67,7 @@ io.sockets.on('connection', function(socket) {
 	
 	socket.on('insert', function() {
 		var data = ['id', '1234', 'mail', 'lee', 970224];
-		mySqlConnection.query("insert into user_auth values ('id','1234','mail','lee',970224)", function(err, rows) {
+		mySqlConnection.query('insert into user_auth values ("id","1234","mail","lee",970224);', function(err, rows) {
 			if (err) {
 				console.error('insert 에러 = ' + err);
 			} else {
